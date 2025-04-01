a7aaa5676fbd5140e8423c38520bc074ca5643f7
pradeepjayathilaka <pradeepjayathilaka06@gmail.com>
2025-04-01 11:29:38 +0530

    Add Firebase configuration and update Gradle files for Kotlin and Google services

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,18 @@
+buildscript {
+    val kotlinVersion = "2.0.0" // Correct Kotlin DSL syntax
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.google.gms:google-services:4.3.15")
+    }
+}  
+
 allprojects {
     repositories {
         google()
